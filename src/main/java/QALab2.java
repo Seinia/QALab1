4aa442f4ce32c46c899561806424b13bf061ba21
Seinia <vlad.yacevilov@gmail.com>
2024-02-22 10:25:23 +0200

    Version 0.4

--- src/main/java/QALab2.java
+++ src/main/java/QALab2.java
@@ -7,6 +7,7 @@ public class QALab2 {
     public static boolean checklines(String[] parts){
         String line = parts[0];
         try {
+            Integer.parseInt(line);
             return true;
         } catch (NumberFormatException e) {
             System.out.printf("Рядок не може бути перетворений в ціле число: %s%n",line);
