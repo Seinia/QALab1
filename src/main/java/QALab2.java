a8e1513a32f0694a6b5dbbd9a51204980d237598
Seinia <vlad.yacevilov@gmail.com>
2024-02-24 18:01:26 +0200

    Version 0.4

--- src/main/java/QALab2.java
+++ src/main/java/QALab2.java
@@ -20,13 +20,17 @@ public class QALab2 {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] parts = line.split("\t");
-                if (checklines(parts)){
-                    if (items.containsKey(parts[0])){
-                        System.out.printf("Товар з кодом - %s вже існує%n",parts[0]);
-                    } else if(items.containsValue(String.format("%s; Виробник - %s",parts[1],parts[2]))){
-                        System.out.printf("Такий товар - %s від виробника %s вже існує%n",parts[1],parts[2]);
-                    } else {
-                        items.put(parts[0], String.format("%s; Виробник - %s", parts[1], parts[2]));
+                if (parts.length>3) {
+                    System.out.println("Вказаний рядок має некоректну кількість частин");
+                }else {
+                    if (checklines(parts)) {
+                        if (items.containsKey(parts[0])) {
+                            System.out.printf("Товар з кодом - %s вже існує%n", parts[0]);
+                        } else if (items.containsValue(String.format("%s; Виробник - %s", parts[1], parts[2]))) {
+                            System.out.printf("Такий товар - %s від виробника %s вже існує%n", parts[1], parts[2]);
+                        } else {
+                            items.put(parts[0], String.format("%s; Виробник - %s", parts[1], parts[2]));
+                        }
                     }
                 }
             }
